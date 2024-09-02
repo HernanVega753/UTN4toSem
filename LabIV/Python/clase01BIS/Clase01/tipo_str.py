17ddfdf4fddcc4401a920ec1fec0742565795a13
Hernán Vega <hervega123@gmail.com>
2024-09-02 20:42:51 -0300

    LabIV | Python | Clase03 | Prof sn y String p2

---
+++ LabIV/Python/clase01BIS/Clase01/tipo_str.py
@@ -0,0 +1,11 @@
+import math
+# Profudizando en el tipo String
+# Concatenación automática en Python
+
+variable = ', Adios'
+mensaje = 'Hola ''Alumnos' + variable
+mensaje += '. Terminamos'
+# print(mensaje)
+
+# Usamos la clase help para ayudar o documentación (build-in)
+help(math.isnan)
