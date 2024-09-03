03ea3918e78afb01a16a0baa4c1cd760506bab36
Hernán Vega <hervega123@gmail.com>
2024-09-03 04:22:40 -0300

    LabIV | JavaScript | Clase3 | ecommerce3

--- LabIV/JavaScript/ecommerce2024/client/js/index.js
+++ LabIV/JavaScript/ecommerce2024/client/js/index.js
@@ -3,26 +3,40 @@ const cart = []; // Carrito. Array vacío
 
 products.forEach(product => {
     const content = document.createElement("div");
+    content.className = "card";
     content.innerHTML = `
     <img src="${product.img}">
     <h3>${product.productName} </h3>
-    <p>$${product.price}</p>
+    <p class="price">${product.price}$</p>
     `;
     shopContent.append(content);
 
     const buyButton = document.createElement("button");
-    buyButton.innerText = "Comprar";
+    buyButton.innerText = "Buy";
 
     content.append(buyButton);
 
     buyButton.addEventListener("click", ()=>{
-        cart.push({
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+
+        if(repeat){
+            cart.map((prod) => {
+                if (prod.id === product.id) {
+                    prod.quanty++
+                }
+            });
+        }else{
+           cart.push({
             id: product.id,
             productName: product.productName,
             price: product.price,
             quanty: product.quanty,
             img: product.img,
-        })
+            }) 
+        }
+
+    
+        
         console.log(cart)
     })
 });
